d93a9d6f50e86b4822681190c55ac5148ad4834b
kimyujin <yujin123.kim@gmail.com>
2022-06-01 23:07:50 +0900

    feat : CPU bound, I/O bound

--- 1/cpu-bound.py
+++ 1/cpu-bound.py
@@ -1,3 +1,8 @@
+# CPU 바운드
+# 1. 프로그램이 실행될 때 실행 속도가 CPU 속도에 의해 제한됨을 의미
+# 2. 정말 복잡한 수학 수식을 계산하는 경우에 컴퓨터의 실행속도가 느려짐
+
+
 def cpu_bound_func(number: int):
     total = 1
     arrange = range(1, number + 1)
